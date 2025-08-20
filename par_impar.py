65f9ec181ca1c711181451e29c3acdffd2313318
Viniciusfini953 <viniciusfini0@gmail.com>
2025-08-20 11:19:33 -0300

    init: iniciando o estudo em Python

---
+++ par_impar.py
@@ -0,0 +1,7 @@
+# declarar a variável que irá guardar um numéro
+valor = 12
+
+if(valor % 2 == 0):
+    print("este valor é par")
+else:
+    print ("este valor é impar")
---
+++ pontoevirgula.py
@@ -0,0 +1,5 @@
+#declaração de variaveis com
+# conteudo e separação utilizando
+# ponto e virgula
+pais = "brasil" ; uf = "sp" ; cidade = "suzano" ; espaço = "-"
+print(pais + espaço + uf + espaço + cidade)
